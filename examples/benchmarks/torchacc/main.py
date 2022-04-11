76f1c5fc7b8bb5fb52bfb3e690d7257d4ec9952d
chengyu.wcy <chengyu.wcy@alibaba-inc.com>
2022-04-11 15:54:06 +0800

    update benchmarks, init for clue

--- examples/benchmarks/torchacc/main.py
+++ examples/benchmarks/torchacc/main.py
@@ -1,3 +1,18 @@
+# coding=utf-8
+# Copyright (c) 2020 Alibaba PAI team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
 import os
 
@@ -9,7 +24,6 @@ print('*'*50)
 print('running local main...\n')
 from easynlp.core import Trainer
 from easynlp.appzoo import get_application_evaluator
-# from easynlp.core.trainer_vanilla import VanillaTrainer as Trainer
 
 from easynlp.appzoo import ClassificationDataset
 from easynlp.appzoo.sequence_classification.model import SequenceClassification
