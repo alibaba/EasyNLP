2f8c35dd0ed350699286d37b3219538df4d44197
Minghui <minghuiqiu@gmail.com>
2022-05-06 19:22:06 +0800

    Update run_all_tests_cpu.sh

--- tests/run_all_tests_cpu.sh
+++ tests/run_all_tests_cpu.sh
@@ -22,6 +22,35 @@ fi
 rm -rf *.tsv *.csv *.txt
 
 
+echo "================== Test user defined vectorization =================="
+# check vectorization
+sh run_vectorization.sh
+
+# Install easynlp cli
+cd ../
+pip uninstall easynlp -y
+python setup.py install --user
+cd tests/
+
+if [ ! -d ./tmp ]; then
+  mkdir tmp/
+fi
+
+rm -rf *.tsv *.csv *.txt
+
+echo "================== Test TorchACC =================="
+python test_torchacc.py
+rm -rf *.tsv
+
+echo "================== Feature Vectorization =================="
+if [ ! -f ./dev2.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/dev2.tsv
+fi
+
+python test_vectorization.py
+rm -rf *.tsv
+
+
 # Unit tests
 echo "================== Test user defined example =================="
 if [ ! -f ./train.tsv ]; then
