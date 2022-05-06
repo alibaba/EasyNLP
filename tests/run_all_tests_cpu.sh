fe0d4d3d65c39c47dcc55c3f9d6f2fd9c5f5785f
Minghui <minghuiqiu@gmail.com>
2022-05-06 18:34:39 +0800

    Update run_all_tests_cpu.sh

--- tests/run_all_tests_cpu.sh
+++ tests/run_all_tests_cpu.sh
@@ -21,12 +21,39 @@ fi
 
 rm -rf *.tsv *.csv *.txt
 
+
 # Unit tests
-echo "================== Test classification =================="
+echo "================== Test user defined example =================="
 if [ ! -f ./train.tsv ]; then
   wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/train.tsv
   wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/dev.tsv
 fi
 
-python test_classification.py
-rm -rf classification_model
+python test_classification_self_defined.py
+rm -rf *.tsv
+
+
+echo "================== Test language modeling =================="
+if [ ! -f ./train.json ]; then
+  wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/language_modeling/train.json
+  wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/language_modeling/dev.json
+fi
+
+python test_language_modeling.py
+rm -rf tmp/*
+rm -rf *.json *.csv *.tsv
+
+echo "================== Test data augmentation =================="
+python test_data_augmentation.py
+rm -rf *.tsv
+
+# # echo "================== Test few shot =================="
+# # python test_few_shot.py
+# # rm -rf few_shot_model few_shot_logs -f
+# # rm -rf *.tsv -f
+
+#
+# uninstall easynlp
+#
+pip uninstall easynlp -y
+
