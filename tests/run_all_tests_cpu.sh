8d19b3c6f0fd313802b653e2f21c743b89199c5a
Minghui <minghuiqiu@gmail.com>
2022-05-06 16:37:13 +0800

    Update run_all_tests_cpu.sh

--- tests/run_all_tests_cpu.sh
+++ tests/run_all_tests_cpu.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 conda create -n myenv python=3.6 
 
-source activate myenv
+conda activate myenv
 
 which python
 python --version
+
+set -e
+export CUDA_VISIBLE_DEVICES=-1
+
+# Cleanup environment
+rm -rf $HOME/.easynlp_modelzoo
+rm -rf $HOME/.cache/huggingface
+
+export MKL_THREADING_LAYER=GNU
+
+echo "================== Test user defined vectorization =================="
+# check vectorization
+sh run_vectorization.sh
