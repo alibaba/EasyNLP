52bb40380c0debddd96a4863feece4e50ab9de57
德灵 <deling.sc@alibaba-inc.com>
2022-07-21 14:37:53 +0800

    add text_vision main.py and bash code

--- examples/appzoo_tutorials/sequence_classification_multilabel/run_train_eval_predict_user_defined_local.sh
+++ examples/appzoo_tutorials/sequence_classification_multilabel/run_train_eval_predict_user_defined_local.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES=$1
 
 if [ ! -f ./train_multilabel_zh.csv ]; then
-  wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/modelhub/nlu/general_news_classification/data/train.csv
+  wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/train_multilabel_zh.csv
 fi
 
 if [ ! -f ./dev_multilabel_zh.csv ]; then
-  wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/modelhub/nlu/general_news_classification/data/dev.csv:
+  wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/dev_multilabel_zh.csv
 fi
 
 MASTER_ADDR=localhost
@@ -23,11 +23,11 @@ if [ "$mode" = "train" ]; then
   python -m torch.distributed.launch $DISTRIBUTED_ARGS main.py \
     --mode=$mode \
     --worker_gpu=1 \
-    --tables=train.csv,dev.csv \
+    --tables=train_multilabel_zh.csv,dev_multilabel_zh.csv \
     --input_schema=content_seq:str:1,label:str:1 \
     --first_sequence=content_seq \
     --label_name=label \
-    --label_enumerate_values=母婴,三农,科学,美文,科技,时尚,房产,美食,艺术,职场,健康,财经,国际,家居,娱乐,文化,教育,游戏,读书,动漫,体育,旅游,汽车,搞笑,健身,宠物,育儿 \
+    --label_enumerate_values=体积大小,外观,制热范围,制热效果,衣物烘干,味道,产品功耗,滑轮提手,声音 \
     --checkpoint_dir=./multi_label_classification_model \
     --learning_rate=3e-5  \
     --epoch_num=1  \
@@ -38,6 +38,7 @@ if [ "$mode" = "train" ]; then
     --app_name=text_classify \
     --user_defined_parameters='
         pretrain_model_name_or_path=hfl/chinese-roberta-wwm-ext
+        multi_label=True
     '
 
 elif [ "$mode" = "evaluate" ]; then
@@ -45,24 +46,26 @@ elif [ "$mode" = "evaluate" ]; then
   python -m torch.distributed.launch $DISTRIBUTED_ARGS main.py \
   --mode=$mode \
   --worker_gpu=1 \
-  --tables=dev.csv \
+  --tables=dev_multilabel_zh.csv \
   --input_schema=content_seq:str:1,label:str:1 \
   --first_sequence=content_seq \
   --label_name=label \
-  --label_enumerate_values=母婴,三农,科学,美文,科技,时尚,房产,美食,艺术,职场,健康,财经,国际,家居,娱乐,文化,教育,游戏,读书,动漫,体育,旅游,汽车,搞笑,健身,宠物,育儿 \
+  --label_enumerate_values=体积大小,外观,制热范围,制热效果,衣物烘干,味道,产品功耗,滑轮提手,声音 \
   --checkpoint_dir=./multi_label_classification_model \
   --sequence_length=128 \
   --micro_batch_size=32 \
   --app_name=text_classify \
-  --user_defined_parameters=''
+  --user_defined_parameters='
+        multi_label=True
+      '
 
 elif [ "$mode" = "predict" ]; then
 
   python -m torch.distributed.launch $DISTRIBUTED_ARGS main.py \
   --mode=$mode \
   --worker_gpu=1 \
-  --tables=dev.csv \
-  --outputs=dev.pred.csv \
+  --tables=dev_multilabel_zh.csv \
+  --outputs=dev_multilabel_zh.pred.csv \
   --input_schema=content_seq:str:1,label:str:1 \
   --output_schema=predictions,probabilities,logits,output \
   --append_cols=label \
@@ -71,6 +74,8 @@ elif [ "$mode" = "predict" ]; then
   --micro_batch_size=32 \
   --sequence_length=128 \
   --app_name=text_classify \
-  --user_defined_parameters=''
+  --user_defined_parameters='
+        multi_label=True
+      '
 
 fi
