60f6e78cbdc64cfd7902e7ae3634df0e92e9d6cc
chywang <chengyu.wcy@alibaba-inc.com>
2022-11-19 22:57:11 +0800

    Update clip_score.sh

--- examples/clip_score/clip_score.sh
+++ examples/clip_score/clip_score.sh
@@ -1,18 +1,10 @@
-export CUDA_VISIBLE_DEVICES=$1
-
-#动态获取GPU数
-OLD_IFS="$IFS"
-IFS="," 
-arr=($1)
-IFS="$OLD_IFS"
-
 if [ ! -f ./MUGE_MR_valid_base64_part.tsv ]; then
   wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/CLIP/MUGE_MR_valid_base64_part.tsv
 fi
 
 easynlp \
   --mode evaluate \
-  --worker_gpu=${#arr[@]} \
+  --worker_gpu=1 \
   --tables=./MUGE_MR_valid_base64_part.tsv \
   --input_schema=text:str:1,image:str:1 \
   --first_sequence=text \
@@ -23,6 +15,5 @@ easynlp \
   --save_checkpoint_steps=500 \
   --sequence_length=32 \
   --micro_batch_size=32 \
-  --app_name=wukong \
+  --app_name=wukong_clip \
   --user_defined_parameters='cosine_similarity=True'
-
