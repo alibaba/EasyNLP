caf6da7398c6fb8762d77ec466680ae2691c4ad5
chywang <chengyu.wcy@alibaba-inc.com>
2022-11-23 10:23:12 +0800

    Update run_all_tests_cpu.sh

--- tests/run_all_tests_cpu.sh
+++ tests/run_all_tests_cpu.sh
@@ -22,9 +22,9 @@ fi
 rm -rf *.tsv *.csv *.txt
 
 
-echo "================== Test user defined vectorization =================="
+# echo "================== Test user defined vectorization =================="
 # check vectorization
-sh run_vectorization.sh
+# sh run_vectorization.sh
 
 # Install easynlp cli
 cd ../
@@ -42,13 +42,13 @@ echo "================== Test TorchACC =================="
 python test_torchacc.py
 rm -rf *.tsv
 
-echo "================== Feature Vectorization =================="
-if [ ! -f ./dev2.tsv ]; then
-  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/dev2.tsv
-fi
+# echo "================== Feature Vectorization =================="
+# if [ ! -f ./dev2.tsv ]; then
+#  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/classification/dev2.tsv
+# fi
 
-python test_vectorization.py
-rm -rf *.tsv
+# python test_vectorization.py
+# rm -rf *.tsv
 
 
 # Unit tests
