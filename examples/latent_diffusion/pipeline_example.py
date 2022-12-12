fecf989d8e79a917d4e84cd9b7b9f1fa58d2293e
chywang <chengyu.wcy@alibaba-inc.com>
2022-12-12 17:44:33 +0800

    Update pipeline_example.py

--- examples/latent_diffusion/pipeline_example.py
+++ examples/latent_diffusion/pipeline_example.py
@@ -28,7 +28,7 @@ def image_to_base64(img):
 
 # # poem
 # generator = pipeline('chinese-ldm-poem',pipeline_params={"n_samples":1,"do_sr":True})
-# data = ['一行白鹭上青天']
+# data = ['停车坐爱枫林晚，霜叶红于二月花']
 
 # # anime
 # generator = pipeline('chinese-ldm-anime',pipeline_params={"n_samples":1,"do_sr":True})
@@ -40,7 +40,7 @@ def image_to_base64(img):
 
 # food
 generator = pipeline('chinese-ldm-food',pipeline_params={"n_samples":1,"do_sr":True})
-data = ['青椒炒牛肉']
+data = ['小炒黄牛肉']
 
 # 生成结果
 result=generator(data)
