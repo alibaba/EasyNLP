36fca42784f62cdc2d94c0402d765df51c3849fb
zjy <756628577@qq.com>
2022-12-12 16:58:12 +0800

    add ie data

--- examples/information_extraction/run_train_eval_predict_appzoo_cli_local.sh
+++ examples/information_extraction/run_train_eval_predict_appzoo_cli_local.sh
@@ -1,14 +1,34 @@
 export CUDA_VISIBLE_DEVICES=$1
 
+if [ ! -f ./train.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/train.tsv
+fi
+
+if [ ! -f ./train_part.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/train_part.tsv
+fi
+
+if [ ! -f ./dev.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/dev.tsv
+fi
+
+if [ ! -f ./predict_input_EE.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/predict_input_EE.tsv
+fi
+
+if [ ! -f ./predict_input_NER.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/predict_input_NER.tsv
+fi
+
 mode=$2
 
 if [ "$mode" = "train" ]; then
 
-  easynlp
+  easynlp \
     --mode $mode \
-    --tables=train.tsv,dev.tsv \
+    --tables=train_part.tsv,dev.tsv \
     --input_schema=id:str:1,instruction:str:1,start:str:1,end:str:1,target:str:1 \
-    --worker_gpu=4 \
+    --worker_gpu=1 \
     --app_name=information_extraction \
     --sequence_length=512 \
     --weight_decay=0.0 \
@@ -16,7 +36,7 @@ if [ "$mode" = "train" ]; then
     --checkpoint_dir=./information_extraction_model/ \
     --data_threads=5 \
     --user_defined_parameters='pretrain_model_name_or_path=hfl/macbert-large-zh' \
-    --save_checkpoint_steps=500 \
+    --save_checkpoint_steps=50 \
     --gradient_accumulation_steps=8 \
     --epoch_num=3  \
     --learning_rate=2e-05  \
@@ -24,11 +44,11 @@ if [ "$mode" = "train" ]; then
 
 elif [ "$mode" = "evaluate" ]; then
 
-  easynlp
+  easynlp \
     --mode $mode \
     --tables=dev.tsv \
     --input_schema=id:str:1,instruction:str:1,start:str:1,end:str:1,target:str:1 \
-    --worker_gpu=4 \
+    --worker_gpu=1 \
     --app_name=information_extraction \
     --sequence_length=512 \
     --weight_decay=0.0 \
@@ -38,12 +58,12 @@ elif [ "$mode" = "evaluate" ]; then
 
 elif [ "$mode" = "predict" ]; then
 
-  easynlp
+  easynlp \
     --mode=$mode \
     --tables=predict_input_EE.tsv,predict_output_EE.tsv \
     --input_schema=id:str:1,scheme:str:1,content:str:1 \
     --output_schema=id,content,q_and_a \
-    --worker_gpu=4 \
+    --worker_gpu=1 \
     --app_name=information_extraction \
     --sequence_length=512 \
     --weight_decay=0.0 \
@@ -52,3 +72,6 @@ elif [ "$mode" = "predict" ]; then
     --data_threads=5 \
     --user_defined_parameters='task=EE'
 fi
+
+#mode=train处，目前使用的是部分训练数据，如果需要使用全部训练数据，请将train_part.tsv修改为train.tsv，save_checkpoint_steps=50修改为save_checkpoint_steps=500
+#mode=predict出，目前验证的是EE任务的一个例子，如果需要验证NER任务，请将predict_input_EE.tsv修改为predict_input_NER.tsv，将predict_output_EE.tsv修改为predict_output_NER.tsv，将task=EE修改为task=NER
--- examples/information_extraction/run_train_eval_predict_user_defined_local.sh
+++ examples/information_extraction/run_train_eval_predict_user_defined_local.sh
@@ -1,7 +1,27 @@
 export CUDA_VISIBLE_DEVICES=$1
 
+if [ ! -f ./train.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/train.tsv
+fi
+
+if [ ! -f ./train_part.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/train_part.tsv
+fi
+
+if [ ! -f ./dev.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/dev.tsv
+fi
+
+if [ ! -f ./predict_input_EE.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/predict_input_EE.tsv
+fi
+
+if [ ! -f ./predict_input_NER.tsv ]; then
+  wget http://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/ie/predict_input_NER.tsv
+fi
+
 MASTER_ADDR=localhost
-MASTER_PORT=6018
+MASTER_PORT=6022
 GPUS_PER_NODE=4
 NNODES=1
 NODE_RANK=0
@@ -14,7 +34,7 @@ if [ "$mode" = "train" ]; then
 
   python -m torch.distributed.launch $DISTRIBUTED_ARGS main.py \
     --mode $mode \
-    --tables=train.tsv,dev.tsv \
+    --tables=train_part.tsv,dev.tsv \
     --input_schema=id:str:1,instruction:str:1,start:str:1,end:str:1,target:str:1 \
     --worker_gpu=4 \
     --app_name=information_extraction \
@@ -24,7 +44,7 @@ if [ "$mode" = "train" ]; then
     --checkpoint_dir=./information_extraction_model/ \
     --data_threads=5 \
     --user_defined_parameters='pretrain_model_name_or_path=hfl/macbert-large-zh' \
-    --save_checkpoint_steps=500 \
+    --save_checkpoint_steps=50 \
     --gradient_accumulation_steps=8 \
     --epoch_num=3  \
     --learning_rate=2e-05  \
@@ -48,7 +68,7 @@ elif [ "$mode" = "predict" ]; then
 
   python -m torch.distributed.launch $DISTRIBUTED_ARGS main.py \
     --mode=$mode \
-    --tables=predict_input_EE.tsv,predict_output_EE.tsv \
+    --tables=predict_input_NER.tsv,predict_output_NER.tsv \
     --input_schema=id:str:1,scheme:str:1,content:str:1 \
     --output_schema=id,content,q_and_a \
     --worker_gpu=4 \
@@ -58,5 +78,9 @@ elif [ "$mode" = "predict" ]; then
     --micro_batch_size=4 \
     --checkpoint_dir=./information_extraction_model/ \
     --data_threads=5 \
-    --user_defined_parameters='task=EE'
+    --user_defined_parameters='task=NER'
+
 fi
+
+#mode=train处，目前使用的是部分训练数据，如果需要使用全部训练数据，请将train_part.tsv修改为train.tsv，save_checkpoint_steps=50修改为save_checkpoint_steps=500
+#mode=predict出，目前验证的是EE任务的一个例子，如果需要验证NER任务，请将predict_input_EE.tsv修改为predict_input_NER.tsv，将predict_output_EE.tsv修改为predict_output_NER.tsv，将task=EE修改为task=NER
