bdc8a826f97fab974ac896fcfb8bb68e6f41632b
deplay <1150696014@qq.com>
2022-12-19 14:09:23 +0800

    fix text2video_retrieval tutorial

--- examples/text2video_retrieval/preprocess_video_frame.sh
+++ examples/text2video_retrieval/preprocess_video_frame.sh
@@ -1,25 +1,21 @@
 # Download data
-if [ ! -f ./msrvtt_data/MSRVTT_data.json ]; then
-    wget https://github.com/ArrowLuo/CLIP4Clip/releases/download/v0.0/msrvtt_data.zip
-    wget https://www.robots.ox.ac.uk/~maxbain/frozen-in-time/data/MSRVTT.zip
-    unzip msrvtt_data.zip
-    unzip MSRVTT.zip -d msrvtt_data/
-    rm msrvtt_data.zip
-    rm MSRVTT.zip
+if [ ! -f ./msrvtt_subset/MSRVTT_data.json ]; then
+    wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/text2video_retrieval/MSRVTT_subset.zip
+    unzip MSRVTT_subset.zip -d msrvtt_subset/
 fi
 
 python preprocess_video_frame.py \
-    --csv_dir=./msrvtt_data/MSRVTT_train.9k.csv \
-    --json_dir=./msrvtt_data/MSRVTT_data.json \
-    --video_dir=./msrvtt_data/MSRVTT/videos/all \
+    --csv_dir=./msrvtt_subset/MSRVTT_train_subset_100.csv \
+    --json_dir=./msrvtt_subset/MSRVTT_data.json \
+    --video_dir=./msrvtt_subset/MSRVTT_video_subset \
     --frame_num=12 \
-    --frame_dir=./msrvtt_data/MSRVTT_extracted_frames \
-    --output=./msrvtt_data/MSRVTT_train.tsv
+    --frame_dir=./msrvtt_subset/MSRVTT_extracted_frames \
+    --output=./msrvtt_subset/MSRVTT_train.tsv
 
 python preprocess_video_frame.py \
-    --csv_dir=./msrvtt_data/MSRVTT_JSFUSION_test.csv \
-    --json_dir=./msrvtt_data/MSRVTT_data.json \
-    --video_dir=./msrvtt_data/MSRVTT/videos/all \
+    --csv_dir=./msrvtt_subset/MSRVTT_test_subset_100.csv \
+    --json_dir=./msrvtt_subset/MSRVTT_data.json \
+    --video_dir=./msrvtt_subset/MSRVTT_video_subset \
     --frame_num=12 \
-    --frame_dir=./msrvtt_data/MSRVTT_extracted_frames \
-    --output=./msrvtt_data/MSRVTT_test_all.tsv
+    --frame_dir=./msrvtt_subset/MSRVTT_extracted_frames \
+    --output=./msrvtt_subset/MSRVTT_test.tsv
