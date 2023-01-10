e7b16f5c9aafe6d4ed8c84901370055f0d91444b
chywang <chengyu.wcy@alibaba-inc.com>
2023-01-10 21:52:45 +0800

    Update run_latent_diffusion_appzoo_cli_local.sh

--- examples/latent_diffusion/run_latent_diffusion_appzoo_cli_local.sh
+++ examples/latent_diffusion/run_latent_diffusion_appzoo_cli_local.sh
@@ -1,33 +1,28 @@
-export CUDA_VISIBLE_DEVICES=$1
-mode=$2
-
-# Local training example
-cur_path=$PWD/../../
-cd ${cur_path}
+mode=$1
 
 # Download data
-if [ ! -f ./tmp/MUGE_train_text_imgbase64.tsv ]; then
-    wget  -P ./tmp https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/painter_text2image/MUGE_train_text_imgbase64.tsv
-    wget  -P ./tmp https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/painter_text2image/MUGE_val_text_imgbase64.tsv
-    wget  -P ./tmp https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/painter_text2image/MUGE_test.text.tsv
+if [ ! -f T2I_train.tsv ]; then
+    wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/painter_text2image/T2I_train.tsv
+    wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/painter_text2image/T2I_val.tsv
+    wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/painter_text2image/T2I_test.tsv
 fi
 
-if [ "$mode" = "finetune" ]; then
+if [ "$mode" = "train" ]; then
   easynlp \
     --mode=train \
     --worker_gpu=1 \
-    --tables=./tmp/MUGE_train_text_imgbase64.tsv,./tmp/MUGE_val_text_imgbase64.tsv \
+    --tables=T2I_train.tsv,T2I_val.tsv \
     --input_schema=idx:str:1,text:str:1,imgbase64:str:1 \
     --first_sequence=text \
     --second_sequence=imgbase64 \
-    --checkpoint_dir=./tmp/finetune_model \
+    --checkpoint_dir=./finetune_model \
     --learning_rate=4e-5 \
-    --epoch_num=20 \
+    --epoch_num=5 \
     --random_seed=42 \
     --logging_steps=100 \
     --save_checkpoint_steps=1000 \
     --sequence_length=288 \
-    --micro_batch_size=32 \
+    --micro_batch_size=2 \
     --app_name=latent_diffusion \
     --user_defined_parameters='
         pretrain_model_name_or_path=alibaba-pai/pai-diffusion-general-slim-zh
@@ -39,14 +34,14 @@ if [ "$mode" = "finetune" ]; then
 
 elif [ "$mode" = "predict" ]; then
   easynlp \
-      --mode predict \
+      --mode=predict \
       --worker_gpu=1 \
-      --tables=./tmp/MUGE_test.text.tsv \
+      --tables=T2I_test.text.tsv \
       --input_schema=idx:str:1,text:str:1 \
       --output_schema=text \
-      --outputs ./output_placeholder.tsv \
+      --outputs=./output_placeholder.tsv \
       --first_sequence=text \
-      --checkpoint_dir=./tmp/finetune_model \
+      --checkpoint_dir=./finetune_model \
       --random_seed=42 \
       --logging_steps=100 \
       --save_checkpoint_steps=500 \
