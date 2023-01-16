4a843521ee733ab78adc702e85486b159662c25f
chywang <chengyu.wcy@alibaba-inc.com>
2023-01-16 17:36:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 setup(
     # Metadata
     name='pai-easynlp',
-    version='0.1.1',
+    version='0.1.2',
     python_requires='>=3.6',
     author='PAI NLP',
     author_email='easynlp@list.alibaba-inc.com',
