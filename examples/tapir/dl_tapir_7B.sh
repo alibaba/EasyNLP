67c549e307a6dbc2b8b78916fd719924335943c5
bug-orz <bohr.y@outlook.com>
2024-11-12 13:09:23 +0800

    Create dl_tapir_7B.sh

---
+++ examples/tapir/dl_tapir_7B.sh
@@ -0,0 +1,9 @@
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/config.json
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/generation_config.json
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/model-00001-of-00003.safetensors
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/model-00001-of-00003.safetensors
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/model-00001-of-00003.safetensors
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/model.safetensors.index.json
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/special_tokens_map.json
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/tokenizer.model
+wget https://atp-modelzoo-sh.oss-cn-shanghai.aliyuncs.com/release/tutorials/TAPIR-Distillation/Tapir-7B/tokenizer_config.json
